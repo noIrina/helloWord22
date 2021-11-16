d1b7cfac60d13adc6fd98664bc2c76b17233a3e7
Irina Slobodianiuk <slobodyanyukia@gmail.com>
2021-11-16 19:23:23 +0300

    новый

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello Word!");
     System.out.println("Line");
+        System.out.println("изменения");
     }
 }
